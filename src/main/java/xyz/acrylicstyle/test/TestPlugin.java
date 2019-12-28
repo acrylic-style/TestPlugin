eee36ad9b9e8c575ebd1f12c47388c626979ed2e
acrylic-style <me@acrylicstyle.xyz>
2019-12-28 12:49:33 +0900

    first commit

---
+++ src/main/java/xyz/acrylicstyle/test/TestPlugin.java
@@ -0,0 +1,26 @@
+package xyz.acrylicstyle.test;
+
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import util.ReflectionHelper;
+import xyz.acrylicstyle.tomeito_core.utils.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestPlugin extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        Log.info("Enabled Test plugin. Enjoy testing.");
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent e) {
+        ReflectionHelper.setFieldWithoutException(EntityExplodeEvent.class, e, "blocks", (List<Block>) new ArrayList<Block>());
+    }
+}
