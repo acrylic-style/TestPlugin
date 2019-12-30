0b5cccc6ad99c816ec0f485492c685c5e34cd545
acrylic-style <me@acrylicstyle.xyz>
2019-12-30 12:55:14 +0900

    aaaa

--- src/main/java/xyz/acrylicstyle/test/TestPlugin.java
+++ src/main/java/xyz/acrylicstyle/test/TestPlugin.java
@@ -4,11 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
 import util.Collection;
 import xyz.acrylicstyle.tomeito_core.utils.Log;
 
@@ -24,18 +28,26 @@ public class TestPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        ArmorStand armorStand = (ArmorStand) e.getPlayer().getWorld().spawnEntity(e.getPlayer().getLocation().add(0, 0, 0), EntityType.ARMOR_STAND);
-        armorStand.setCustomName(ChatColor.LIGHT_PURPLE + e.getPlayer().getName());
-        armorStand.setCustomNameVisible(true);
-        armorStand.setVisible(false);
-        armorStand.setSmall(true);
-        armorStands.add(e.getPlayer().getUniqueId(), armorStand);
-        e.getPlayer().setPassenger(armorStand);
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if (e.getEntityType() == EntityType.VILLAGER) {
+            e.setCancelled(true);
+            return;
+        }
+        if (!(e.getEntity() instanceof Player) || !(e.getDamager() instanceof Player)) return;
+        e.setCancelled(true);
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent e) {
-        armorStands.remove(e.getPlayer().getUniqueId()).remove();
+    public void onEntityDamage(EntityDamageEvent e) {
+        if (e.getEntityType() == EntityType.VILLAGER) {
+            e.setCancelled(true);
+            return;
+        }
+        if (e.getEntityType() != EntityType.PLAYER) return;
+        if (e.getCause() == EntityDamageEvent.DamageCause.FALL && ((Player) e.getEntity()).hasPotionEffect(PotionEffectType.WATER_BREATHING)) {
+            e.setDamage(e.getDamage()*2);
+        } else if (e.getCause() == EntityDamageEvent.DamageCause.VOID) {
+            e.setDamage(100);
+        }
     }
 }
