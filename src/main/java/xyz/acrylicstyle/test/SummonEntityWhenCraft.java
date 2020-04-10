782871c7cef892bcf0c33c5d855af7ce9027f52f
acrylic-style <me@acrylicstyle.xyz>
2020-04-10 18:34:30 +0900

    ok

---
+++ src/main/java/xyz/acrylicstyle/test/SummonEntityWhenCraft.java
@@ -0,0 +1,25 @@
+package xyz.acrylicstyle.test;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import util.ICollectionList;
+
+public class SummonEntityWhenCraft extends JavaPlugin implements Listener {
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onCraftItem(CraftItemEvent e) {
+        Location location = e.getViewers().get(0).getLocation();
+        World world = location.getWorld();
+        assert world != null;
+        world.spawnEntity(location, ICollectionList.asList(EntityType.values()).filter(t -> t != EntityType.PLAYER && t.isSpawnable()).shuffle().first());
+    }
+}
